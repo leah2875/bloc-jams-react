b456d5f4ecc2326f595acd0fd494db1b2951bca9
Leah Weile <leahweile@hotmail.com>
2019-02-01 22:36:24 -0800

    Checkpoint 3 Routing: completed

---
+++ src/components/Landing.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Landing = () => <section className='landing'>Landing page will go here</section>;
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Library = () => <section className='library'>Library will go here</section>;
+
+export default Library;
